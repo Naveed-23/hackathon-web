7b7b51eb7e510668c23f12e14119ffaecd38999a
Naveed-23 <naveedbh23@gmail.com>
2024-09-06 21:57:16 +0530

    landing content

--- src/components/LandingContent.tsx
+++ src/components/LandingContent.tsx
@@ -7,29 +7,32 @@ const LandingContent = () => {
   return (
     <div className='bg-darkBg px-28 py-20'>
       <main className="flex items-center justify-center mb-12">
-        <div className="absolute mr-[700px] mt-[184px] ml-[120px] mb-[93px] p-4">
+        <div className="absolute flex mr-[700px] mt-[184px] ml-[120px] mb-[93px] p-4">
+          <div className="h-28 w-4 bg-yellow-500 mr-8"></div>
+          <div>
             <h1 className="text-5xl font-medium text-white mb-6">
-                Accelerate Innovation with Global AI Challenges
+              Accelerate Innovation with Global AI Challenges
             </h1>
             <div className=''>
-            <p className="text-lg text-gray-300 mb-8">
+              <p className="text-lg text-gray-300 mb-8">
                 AI Challenges at DPhi simulate real-world problems. It is a great place to put your AI/Data Science skills to test on diverse datasets allowing you to foster learning through competitions.
-            </p>
+              </p>
             </div>
             <button onClick={() => {
               navigate('/create-challenge')
             }} className="bg-Slatetxt hover:bg-slate-200 text-darkBg font-semibold py-2 px-6 rounded-lg">
-                Create Challenge
+              Create Challenge
             </button>
+          </div>
         </div>
+
+        {/* Rocket Image */}
         <div className="w-3/4 flex justify-end items-center">
-            <img src={Rocket} alt="Rocket" className="w-80 h-auto" />
+          <img src={Rocket} alt="Rocket" className="w-80 h-auto" />
         </div>
       </main>
     </div>
   );
 };
-
-
 
 export default LandingContent;
